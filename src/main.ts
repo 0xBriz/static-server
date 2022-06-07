95744f0778af1bf058d28127c080b1dedde36c99
0xBriz <jaybrizle@gmail.com>
2022-06-07 14:05:21 -0400

    env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import { join } from 'path';
 
 async function bootstrap() {
+  config({ path: join(process.cwd(), '.env') });
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.HTTP_PORT || 3535;
-  await app.listen(PORT, () => console.log(`Static Server listening on: ${PORT}`));
+  await app.listen(PORT, () =>
+    console.log(`Static Server listening on: ${PORT}`),
+  );
 }
 
 bootstrap();
