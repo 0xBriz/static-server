a4ed8767037c63d871dfa9f0a54370facc2c7647
0xBriz <jaybrizle@gmail.com>
2022-06-08 17:18:51 -0400

    compression

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import { join } from 'path';
+import * as compression from 'compression';
 
 async function bootstrap() {
   config({ path: join(process.cwd(), '.env') });
   const app = await NestFactory.create(AppModule);
+  app.use(compression());
+
   const PORT = process.env.HTTP_PORT || 80;
   await app.listen(PORT, () =>
     console.log(`Static Server listening on: ${PORT}`),
